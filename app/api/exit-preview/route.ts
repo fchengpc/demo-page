23e6df300a79d42ce25b21b5d7928b71df01b059
fcheng <6640631+fchengpc@users.noreply.github.com>
2024-02-23 14:36:59 -0500

    general project fix

--- app/api/exit-preview/route.ts
+++ app/api/exit-preview/route.ts
@@ -1,5 +1,7 @@
 import { exitPreview } from '@prismicio/next';
 
+export const dynamic = 'force-static'; // force into static build
+
 export function GET() {
 	return exitPreview();
 }
--- app/api/preview/route.ts
+++ app/api/preview/route.ts
@@ -3,8 +3,11 @@ import { redirectToPreviewURL } from '@prismicio/next';
 
 import { createClient } from '../../../prismicio';
 
+export const dynamic = 'force-static'; // force into static build
+
 export async function GET(request: NextRequest) {
 	const client = createClient();
 
+	// eslint-disable-next-line @typescript-eslint/return-await
 	return await redirectToPreviewURL({ client, request });
 }
